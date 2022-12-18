9aa8a9d1177884b2c90adc94d25a9ade7974b2d5
giyeonna@gmail.com <giyeonna@gmail.com>
2022-12-18 22:08:01 +0900

    Add : PlayerController

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 dir;
+    public float speed;
+
+    //0:left 1:mid 2:right
+    private int line = 1;
+    public float lineDistance = 4;
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir.z = speed;
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (line == 2) return;
+            line++;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (line == 0) return;
+            line--;
+        }
+
+        Vector3 targetPos = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if(line == 0)
+        {
+            targetPos += Vector3.left * lineDistance;
+        }
+        else if(line == 2)
+        {
+            targetPos += Vector3.right * lineDistance;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, 80 * Time.fixedDeltaTime);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(dir * Time.fixedDeltaTime);
+    }
+}
